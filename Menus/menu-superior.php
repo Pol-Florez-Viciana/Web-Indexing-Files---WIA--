96c3b45f357fb1af7d7a93fda6db8d71c039428f
Pol Flórez Viciana <38541039+Pol-Florez-Viciana@users.noreply.github.com>
2019-03-13 16:46:46 +0100

    Add files via upload

--- Menus/menu-superior.php
+++ Menus/menu-superior.php
@@ -1,12 +1,13 @@
 ﻿<?php
+
+echo('<form action="buscar.php" method="POST"  style="display: inline-block;" >');
+echo('<input style="display: inline-block;" id="Search" name="Search" type="text" value="" />');
+echo('<input style="display: inline-block;" id="submit" type="submit" value="Buscar" />');
+echo('</form> ');
+
 // Ubicación de la Carpeta Raíz Principal
 $Dir = "./Raiz-Web-Principal/";
 $MisSecciones = GetLinkSections($Dir,"raiz-principal.php");
-echo($MisSecciones);
-
-// Ubicación de la Carpeta Raíz Secundaria
-$Dir = "./Raiz-Web-Aplicaciones/";
-$MisSecciones = GetLinkSections($Dir,"aplicaciones.php");
 echo($MisSecciones);
 
 // Ubicación de la Carpeta Raíz Secundaria
